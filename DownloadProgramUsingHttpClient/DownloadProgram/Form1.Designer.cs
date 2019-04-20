3c1309cc175c83be983b2098db038e7dee4b2146
User <User@DESKTOP-I87ABNR>
2019-04-20 10:59:50 -0700

    Add New Solution

--- DownloadProgramUsingHttpClient/DownloadProgram/Form1.Designer.cs
+++ DownloadProgramUsingHttpClient/DownloadProgram/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Download = new System.Windows.Forms.Button();
+            this.TextUri = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.TextProgress = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Download
+            // 
+            this.Download.Location = new System.Drawing.Point(210, 27);
+            this.Download.Name = "Download";
+            this.Download.Size = new System.Drawing.Size(75, 23);
+            this.Download.TabIndex = 0;
+            this.Download.Text = "Download";
+            this.Download.UseVisualStyleBackColor = true;
+            this.Download.Click += new System.EventHandler(this.Download_Click);
+            // 
+            // TextUri
+            // 
+            this.TextUri.Location = new System.Drawing.Point(100, 29);
+            this.TextUri.Name = "TextUri";
+            this.TextUri.Size = new System.Drawing.Size(100, 20);
+            this.TextUri.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter UrI";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(100, 64);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 19);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // TextProgress
+            // 
+            this.TextProgress.Location = new System.Drawing.Point(210, 64);
+            this.TextProgress.Name = "TextProgress";
+            this.TextProgress.Size = new System.Drawing.Size(75, 20);
+            this.TextProgress.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.TextProgress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextUri);
+            this.Controls.Add(this.Download);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Download;
+        private System.Windows.Forms.TextBox TextUri;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox TextProgress;
     }
 }
 
--- DownloadProgramUsingHttpClient/DownloadProgram/Form1.cs
+++ DownloadProgramUsingHttpClient/DownloadProgram/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,60 @@ namespace DownloadProgram
 {
     public partial class Form1 : Form
     {
+        static int Count = 1;
+        string DownloadFileUrl;
+        string DestinationFilePath;
+        string NewPath;
         public Form1()
         {
             InitializeComponent();
+        }
+        private async void Download_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DownloadFileUrl = TextUri.Text;
+                DestinationFilePath = Path.GetFullPath($"NewFile{Count}.zip");
+                Count++;
+            }
+            catch (UriFormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            catch (NotSupportedException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+            using (var client = new HttpClientDownloadWithProgress(DownloadFileUrl, DestinationFilePath))
+            {
+                client.ProgressChanged += (totalFileSize, totalBytesDownloaded, progressPercentage) =>
+                {
+                    TextProgress.Text = $"{progressPercentage}% ({totalBytesDownloaded}/{totalFileSize})";
+                    progressBar1.Value = (int)progressPercentage;
+                };
+                try
+                {
+                    await client.StartDownload();
+                }
+
+                catch (Exception ex)
+                {
+                    TextProgress.Text = ex.Message;
+
+                }
+            }
         }
     }
 }
